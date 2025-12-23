6480d5aff99622f2cdb3397a9bb2851501784037
Pablo Perez <alu0101619002@ull.edu.es>
2025-12-23 11:16:01 +0000

    Pruebas: creadas las pruebas para la creación del objeto

---
+++ videojuegos/videojuegos_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require_relative 'videojuegos'
+
+RSpec.describe Videojuego do
+    before(:each) do 
+        video = Videojuegos::Videojuego.new("League of Legends", "moba", 0.0, {region: :eu, cantidad: 100})
+    end
+
+    context "Pruebas de atributos" do
+        it "deberia tener un nombre" do
+            expect(video.nombre).to eq("League of Legends")
+        end
+
+        it "deberia tener un genero" do
+            expect(video.genero).to eq("moba")
+        end
+
+        it "deberia tener una calificacion" do
+            expect(video.calificacion).to eq(0.0)
+        end
+
+        it "deberia tener servidores" do
+            expect(video.servidores).to eq({region: :eu, cantidad: 100})
+        end
+    end
+end
