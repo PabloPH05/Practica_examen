157b85c2e0fd09766a33a80f9fadbb3887538674
Pablo Perez <alu0101619002@ull.edu.es>
2025-12-28 10:16:58 +0000

    Implementado el constructor de la clase Caballero

---
+++ TorneoMedieval(POO)/torneo.rb
@@ -0,0 +1,11 @@
+class Caballero
+    attr_reader :nombre, :casa, :victorias, :fuerza
+
+    def initialize(nombre, casa, victorias, fuerza)
+        @nombre = nombre
+        @casa = casa
+        @victorias = victorias
+        @fuerza = fuerza
+    end
+end
+        
