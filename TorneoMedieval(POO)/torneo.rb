5dabd5f657ce038a0fb071dab0192d4cf3189179
Pablo Perez <alu0101619002@ull.edu.es>
2025-12-28 11:04:18 +0000

    Implementada la funcionalidad guardia_real

--- TorneoMedieval(POO)/torneo.rb
+++ TorneoMedieval(POO)/torneo.rb
@@ -22,5 +22,12 @@ class Caballero
         mejor_casa = caballeros.group_by{|i| i.casa}.map{|x,y| [x, y.map{|z| z.victorias}.sum]}.max_by{|a,b| b}[0]
         mejor_casa
     end
+
+    def guardia_real(caballeros)
+        caballeros << self
+        los_mejores = caballeros.select{|i| i.fuerza > 8} #Filtrar
+        ordenados = los_mejores.sort_by{|x| x.victorias} #Ordenar
+        resultado = ordenados.map{|i| "#{i.nombre} (#{i.casa})"}
+        resultado
+    end
 end
-        
